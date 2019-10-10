9c2ad24ccb9d67a1569f5aab0f6a95feb7cfc691
dimitramuni <muni.dimitra@gmail.com>
2019-10-11 00:03:25 +0200

    Update app.R

--- app.R
+++ app.R
@@ -6,9 +6,7 @@ volcano_data <- read.delim("https://www.ngdc.noaa.gov/nndc/struts/results?type_0
 volcano_data<-volcano_data[,c("Year","Name","Location","Country","Latitude","Longitude","Type")]
 
 ui <- fluidPage( titlePanel(title="Volcanic Eruptions Map"),
-                 sidebarLayout(sidebarPanel("Co-ordinates"),mainPanel(plotOutput("volcanoplot"))),
-                 
-   
+                 sidebarLayout(sidebarPanel("Coordinates"),mainPanel(plotOutput("volcanoplot"))),
                  
                  sliderInput("lat_slider", "lattitude", min = -90, max = 90,value = 63, pre = "Degree"),
                  sliderInput("long_slider", "longitude", min = -180, max = 180,value =-19, pre = "Degree"),
@@ -19,12 +17,12 @@ ui <- fluidPage( titlePanel(title="Volcanic Eruptions Map"),
 
 server <- function(input, output){
   
-                  output$volcanoplot<- renderPlot(
+                  output$volcanoplot<- renderPlot({
                                           
                                           get_stamenmap(c(left = input$long_slider-10 , bottom =input$lat_slider-10, right =input$long_slider+10, top =input$lat_slider+10), zoom = 5,maptype = "terrain") %>% ggmap()+geom_point(aes(x =volcano_data$Longitude, y =volcano_data$Latitude), data = volcano_data, colour = "red", size = 2)  
-  
+                                           },height =900,width = 700)
                     
-                    )
+                    
 }  
-# Last line
+
 shinyApp(ui = ui, server = server) 
